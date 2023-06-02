b7f9176936c04514f2d909f49832a808b5839843
Frumentius <frumentius_sonic@yahoo.com>
2023-06-02 14:13:05 +0700

    Write another way of subscribe function, for observable-basic

--- src/app/observable-basic/observable-basic.component.ts
+++ src/app/observable-basic/observable-basic.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable, Observer, of, from } from 'rxjs';
 
 @Component({
@@ -6,9 +6,9 @@ import { Observable, Observer, of, from } from 'rxjs';
   templateUrl: './observable-basic.component.html',
   styleUrls: ['./observable-basic.component.css']
 })
-export class ObservableBasicComponent implements OnInit, OnDestroy {
+export class ObservableBasicComponent implements OnInit {
   sequence;
-  constructor(){
+  constructor() {
     /* These are the same way to create an observable. */
     this.sequence = new Observable(this.sequenceSubscriber);
     //this.sequence = of(1,2,3);
@@ -20,19 +20,30 @@ export class ObservableBasicComponent implements OnInit, OnDestroy {
     observer.next(2);
     observer.next(3);
     observer.complete();
-  
+
     // unsubscribe function doesn't need to do anything in this
     // because values are delivered synchronously
-    return {unsubscribe() {}};
+    return { unsubscribe() { } };
   }
 
   ngOnInit() {
     this.sequence.subscribe({
       next(num) { console.log(num); },
+      error(err) { console.log(err); },
       complete() { console.log('Finished sequence'); }
     });
-  }
-  ngOnDestroy(){
-    //if (this.sequence) this.sequence.unsubscribe();
+
+    //Other ways to write this subscribe function.
+    /* this.sequence.subscribe({
+      next: (num: number) => console.log('Observer got a next value: ' + num),
+      error: (err: Error) => console.error('Observer got an error: ' + err),
+      complete: () => console.log('Observer got a complete notification'),
+    }); */
+    //Not the best way to write the function, might be deprecated.
+    /* this.sequence.subscribe(
+      num => console.log('Observer got a next value: ' + num),
+      err => console.error('Observer got an error: ' + err),
+      () => console.log('Observer got a complete notification')
+    ); */
   }
 }
